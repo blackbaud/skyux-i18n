78ab49da26c5ee21c0c58220381d25fa065c0d54
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-17 10:13:03 -0400

    Re-added i18n module

--- src/app/public/index.ts
+++ src/app/public/index.ts
@@ -1,5 +1,6 @@
 export * from './testing';
 export * from './host-locale-provider';
+export * from './i18n.module';
 export * from './locale-provider';
 export * from './resources.pipe';
 export * from './resources.service';
