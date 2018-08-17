ae048e76357de1996a9785b8a26dba681c698c3a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-17 09:59:41 -0400

    Adjusted require context

--- src/app/public/index.ts
+++ src/app/public/index.ts
@@ -1,6 +1,5 @@
 export * from './testing';
 export * from './host-locale-provider';
-export * from './i18n.module';
 export * from './locale-provider';
 export * from './resources.pipe';
 export * from './resources.service';
