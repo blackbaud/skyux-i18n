f7acb58f0727a4e3e22e93f0a0aa5fbfc73a536f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-04-26 09:01:00 -0400

    Updated detector

--- src/app/public/modules/i18n/browser-detector.ts
+++ src/app/public/modules/i18n/browser-detector.ts
@@ -1,7 +1,10 @@
+// Taken from Angular 4's `BrowserDetection` utility:
+// https://github.com/angular/angular/blob/4.4.x/packages/platform-browser/testing/src/browser_util.ts
+
 export class SkyBrowserDetector {
+
   public static isIE = (
-    !!(window as any).MSInputMethodContext &&
-    !!(document as any).documentMode
+    navigator.userAgent.indexOf('Trident') > -1
   );
 
   public static isWindows7 = (
