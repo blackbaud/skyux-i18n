b3c6c7a3e23e994df5a84ab0d562a1f8b3a4e0cb
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-07-25 13:44:32 -0400

    Revert change

--- src/app/public/modules/i18n/browser-detector.ts
+++ src/app/public/modules/i18n/browser-detector.ts
@@ -7,10 +7,6 @@ export class SkyBrowserDetector {
     navigator.userAgent.indexOf('Trident') > -1
   );
 
-  public static isEdge = (
-    navigator.userAgent.indexOf('Edge') > -1
-  );
-
   public static isWindows7 = (
     navigator.userAgent.indexOf('Windows NT 6.1') > -1
   );
