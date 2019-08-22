5182302012cf2f424107eceb3fb62248cb4d94fb
Matthew Bell <Matthew.Bell@blackbaud.me>
2019-08-22 09:07:27 -0400

    Adding resource name provide
    Adding resource name provider to allow SPAs to have a hook in the resource process to control which resource is actually returned for a requested resource name.  This will be used for rebranding terms based on vertical, such as the Church need to rebrand Fundraiser to Relationship manager.

---
+++ src/app/public/modules/i18n/resource-name-provider.spec.ts
@@ -0,0 +1,13 @@
+import {
+  SkyAppResourceNameProvider
+} from './resource-name-provider';
+
+describe('Resource name provider', () => {
+  it('should return the input resouce name', () => {
+    const provider = new SkyAppResourceNameProvider();
+    const exampleResourceName = 'test-resource-name';
+    provider.getResourceName(exampleResourceName).subscribe((name) => {
+      expect(name).toEqual(exampleResourceName);
+    });
+  });
+});
