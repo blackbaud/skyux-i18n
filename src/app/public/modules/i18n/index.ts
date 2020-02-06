887d6c9432d05081fc47f958fa7c676a7124d3a3
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-02-06 11:10:18 -0500

    Remove deep imports

---
+++ src/app/public/modules/i18n/index.ts
@@ -0,0 +1,17 @@
+// Include in the bundle, but not in the root API.
+import './get-string-for-locale';
+import './intl-date-formatter';
+import './intl-number-formatter';
+
+export * from './host-locale-provider';
+export * from './i18n.module';
+export * from './lib-resources-provider';
+export * from './lib-resources-providers-token';
+export * from './lib-resources.pipe';
+export * from './lib-resources.service';
+export * from './locale-info';
+export * from './locale-provider';
+export * from './resource-name-provider';
+export * from './resources.pipe';
+export * from './resources.service';
+export * from './resources';
