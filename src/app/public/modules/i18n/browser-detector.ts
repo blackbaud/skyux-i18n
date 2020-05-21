f79b90307b85f7bd7584666113761a8f8251afd9
Alex Kingman <alex.kingman@blackbaud.me>
2020-05-21 10:13:10 -0400

    added firefox, chrome, edge to browser detector

--- src/app/public/modules/i18n/browser-detector.ts
+++ src/app/public/modules/i18n/browser-detector.ts
@@ -3,12 +3,25 @@
 
 export class SkyBrowserDetector {
 
+  public static isChrome = (
+    navigator.userAgent.indexOf('Chrome') > -1 &&
+    navigator.userAgent.indexOf('Mobile Safari') == -1 &&
+    navigator.userAgent.indexOf('Edge') == -1
+  );
+
+  public static isFirefox = (
+    navigator.userAgent.indexOf('Firefox') > -1
+  );
+
   public static isIE = (
     navigator.userAgent.indexOf('Trident') > -1
+  );
+
+  public static isEdge = (
+    navigator.userAgent.indexOf('Edge') > -1
   );
 
   public static isWindows7 = (
     navigator.userAgent.indexOf('Windows NT 6.1') > -1
   );
-
 }
