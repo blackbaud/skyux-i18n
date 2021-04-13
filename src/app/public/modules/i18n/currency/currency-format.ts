3d7fc5ddbf7734882b9b0ffcd8a2c6f827a5fda7
Thomas Ortiz <thomasortiz95@gmail.com>
2021-04-13 13:46:19 -0400

    Added Currency Format Service (#95)
    * Added packages
    
    * Added missing proj template files
    
    * Added and exported currency format
    
    * Added currency service
    
    * Remove autonumeric references
    
    * Fix doc
    
    * Test stub
    
    * Changed class back to an interface
    
    * Remove currency service
    
    * Removed auto spies
    
    * Changed field and doc to say isoCurrencyCode to be explicit
    
    * Removed SkyAppLocale reference and updated docs
    
    * Revert vscode settings.json
    
    * Revert settings.json to origin/master version explicitly
    
    * Added SkyCurrencySymbolLocation type
    
    * Added consts and removed getCurrencySymbol since it apparently does rely on Angular Locales being loaded
    
    * Added specific IE test
    
    * Remove getCurrencyPrecision
    
    * Unnested the params option into explicit params
    
    * Updated IE shim based off Steve's stackoverflow link

---
+++ src/app/public/modules/i18n/currency/currency-format.ts
@@ -0,0 +1,22 @@
+/**
+ * The formatting options for Currency + Locale.
+ */
+export interface SkyCurrencyFormat {
+  /** The Locale */
+  locale: string;
+  /** The ISO 4217 Currency Code */
+  isoCurrencyCode: string;
+  /** The symbol */
+  symbol: string;
+  /** The symbol's location -- prefix or suffix? */
+  symbolLocation: SkyCurrencySymbolLocation;
+  /** The fractional decimal character  */
+  decimalCharacter: string;
+  /** The grouping character (1,000) */
+  groupCharacter: string;
+  /** The numeric precision (decimal places) */
+  precision: number;
+}
+
+/** The Currency symbol's location */
+export type SkyCurrencySymbolLocation = 'prefix' | 'suffix';
