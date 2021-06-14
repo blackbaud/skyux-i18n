898d6c6fdb7891790ac31d70ed908803f8afc1c3
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-06-14 15:47:28 -0400

    Remove injectable decorator from lib resources provider

--- src/app/public/modules/i18n/lib-resources-provider.ts
+++ src/app/public/modules/i18n/lib-resources-provider.ts
@@ -1,16 +1,11 @@
 // #region imports
 import {
-  Injectable
-} from '@angular/core';
-
-import {
   SkyAppLocaleInfo
 } from './locale-info';
 // #endregion
 
-@Injectable()
 export abstract class SkyLibResourcesProvider {
 
-  public getString: (localeInfo: SkyAppLocaleInfo, name: string) => string;
+  public abstract getString: (localeInfo: SkyAppLocaleInfo, name: string) => string;
 
 }
