1eb9e84c195eecf943e9c47c23c70865dd2afb2b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-06-15 13:29:51 -0400

    Update changelog

--- src/app/public/modules/i18n/browser-detector.ts
+++ src/app/public/modules/i18n/browser-detector.ts
@@ -1,9 +1,6 @@
 // Inspired by Angular 4's `BrowserDetection` utility:
 // https://github.com/angular/angular/blob/4.4.x/packages/platform-browser/testing/src/browser_util.ts
 
-/**
- * @internal
- */
 export class SkyBrowserDetector {
 
   public static isIE = (
