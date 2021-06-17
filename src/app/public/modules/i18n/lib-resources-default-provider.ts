84042d0fb6e01d1ae526c79a64324a8ba1369c58
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-06-17 08:10:00 -0400

    Remove default provider

--- src/app/public/modules/i18n/lib-resources-default-provider.ts
+++
@@ -1,9 +0,0 @@
-import {
-  SkyLibResourcesProvider
-} from './lib-resources-provider';
-
-export class LibResourcesDefaultProvider implements SkyLibResourcesProvider {
-  public getString(): string {
-    return '';
-  }
-}
