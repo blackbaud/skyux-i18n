6ebfd679718c89c7358f16c09f43965586a34d41
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-02 15:16:31 -0400

    Fix build

--- scripts/build-schematics.js
+++ scripts/build-schematics.js
@@ -12,9 +12,22 @@ function buildSchematics() {
     '--project', 'tsconfig.schematics.json'
   ]);
 
+  // Copy collection.json.
   fs.copySync(
     path.join(LIB_PATH, 'schematics/collection.json'),
     path.join('dist/i18n/schematics/collection.json')
+  );
+
+  // Copy schemas.
+  fs.copySync(
+    path.join(LIB_PATH, 'schematics/ng-generate/lib-resources-module/schema.json'),
+    path.join('dist/i18n/schematics/ng-generate/lib-resources-module/schema.json')
+  );
+
+  // Copy template files.
+  fs.copySync(
+    path.join(LIB_PATH, 'schematics/ng-generate/lib-resources-module/files'),
+    path.join('dist/i18n/schematics/ng-generate/lib-resources-module/files')
   );
 
   console.log('Done.');
