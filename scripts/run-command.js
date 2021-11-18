15d622f7b2f4dd76b316358daf686bce7b207f42
John White <john.white@blackbaud.me>
2021-11-18 09:44:32 -0500

    Prettier Release 5.0.1

--- scripts/run-command.js
+++ scripts/run-command.js
@@ -6,7 +6,7 @@ const LIB_PATH = path.join(process.cwd(), 'projects/i18n');
 function runCommand(command, args) {
   crossSpawn.sync(command, args, {
     cwd: LIB_PATH,
-    stdio: 'inherit'
+    stdio: 'inherit',
   });
 }
 
